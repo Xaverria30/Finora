72f4eb6709e6810f8d9f71b326442effa39ec9c6
Jey <galuhajeng2ver16@gmail.com>
2026-04-24 20:58:32 +0700

    nambah user model part 2

---
+++ lib/models/user_model.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) => User(
+  id: json['id'] as String,
+  name: json['name'] as String,
+  email: json['email'] as String,
+  currency: json['currency'] as String,
+  photoUrl: json['photoUrl'] as String?,
+  createdAt: DateTime.parse(json['createdAt'] as String),
+  updatedAt: DateTime.parse(json['updatedAt'] as String),
+);
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+  'id': instance.id,
+  'name': instance.name,
+  'email': instance.email,
+  'currency': instance.currency,
+  'photoUrl': instance.photoUrl,
+  'createdAt': instance.createdAt.toIso8601String(),
+  'updatedAt': instance.updatedAt.toIso8601String(),
+};
